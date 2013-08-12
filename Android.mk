ae8a468601e123a94f67282e0053b4e4c5c4d2ae
PeterCxy <xqsx43cxy@126.com>
2013-08-12 19:45:36 +0800

    Phone: add PhoneLocation.java to static library for other packages to use

--- Android.mk
+++ Android.mk
@@ -1,5 +1,16 @@
 LOCAL_PATH:= $(call my-dir)
 
+# Static library with some common classes for the phone apps.
+# To use it add this line in your Android.mk
+#  LOCAL_STATIC_JAVA_LIBRARIES := com.android.phone.common
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	src/com/android/phone/location/PhoneLocation.java
+
+LOCAL_MODULE := com.android.phoneloc
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
 # Build the Phone app which includes the emergency dialer. See Contacts
 # for the 'other' dialer.
 include $(CLEAR_VARS)
